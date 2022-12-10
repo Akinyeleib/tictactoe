712b03be963b7f5b6e25ac016715d0ee8de690d0
Akinyele Ibrahim <iakinyele3@gmail.com>
2022-12-10 10:09:58 +0100

    Simple appbar designed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // runApp();
+void main() => runApp(const GameActivity());
+
+class GameActivity extends StatefulWidget {
+  const GameActivity({super.key});
+
+  @override
+  State<GameActivity> createState() => _GameActivityState();
+}
+
+class _GameActivityState extends State<GameActivity> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tic Tac Toe"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: const [
+            
+          ],
+        ),
+      ),
+    );
+  }
 }
